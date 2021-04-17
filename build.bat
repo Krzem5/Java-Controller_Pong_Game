bf6d6b497a461d1b866bbffef43486fc31a881ac
Krzem <krzem5.dev@gmail.com>
2021-04-17 13:40:03 +0200

    Push Update 04/17/2021, 13:39:55

--- build.bat
+++ build.bat
@@ -1,5 +1,3 @@
-echo off
-echo NUL>_.class&&del /s /f /q *.class
+@echo off
 cls
-javac -cp com/krzem/controller_game/modules/purejavahidapi.jar;com/krzem/controller_game/modules/jna-4.0.0.jar; com/krzem/controller_game/Main.java&&java -cp com/krzem/controller_game/modules/purejavahidapi.jar;com/krzem/controller_game/modules/jna-4.0.0.jar; com/krzem/controller_game/Main
-start /min cmd /c "echo NUL>_.class&&del /s /f /q *.class"
+python build.py %*
